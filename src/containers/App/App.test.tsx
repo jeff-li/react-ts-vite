ddce125ba211fdbafff4c82697a7a83574cf720d
Jeff Li <jeff.li@autocase.com>
2023-01-18 20:24:31 -0500

    Update to vite 4 with swc

--- src/containers/App/App.test.tsx
+++ src/containers/App/App.test.tsx
@@ -1,7 +1,13 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />
+    </MemoryRouter>);
   expect(screen.getByText('Vite + React')).toBeInTheDocument();
 });
